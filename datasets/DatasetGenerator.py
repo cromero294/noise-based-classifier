c9fb781e04c077711638ea9df563b0105f12912c
Mario <mar294cr@gmail.com>
2019-11-15 19:05:17 +0100

    new datasets added

---
+++ datasets/DatasetGenerator.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+def create_dataset(n, model, ymargin=0.0, noise=None, output_boundary=False):
+    x = np.random.rand(n, 1) * 2.0 * np.pi
+    xbnd = np.linspace(0, 2.0 * np.pi, 100)
+
+    if model == 'sine':
+        y = (np.random.rand(n, 1) - 0.5) * 2.2
+        c = y > np.sin(x)
+        ybnd = np.sin(xbnd)
+    elif model == 'linear':
+        y = np.random.rand(n, 1) * 2.0 * np.pi
+        c = y > x
+        ybnd = xbnd
+    elif model == 'square':
+        y = np.random.rand(n, 1) * 4.0 * np.pi * np.pi
+        c = y > x * x
+        ybnd = xbnd * xbnd
+    elif model == 'ringnorm':
+        a = 2 / np.sqrt(2.0)
+        n2 = int(n / 2)
+        x[:n2] = np.random.normal(0, 4.0, (n2, 1))
+        x[n2:] = np.random.normal(a, 1.0, (n2, 1))
+        y = np.array(x)
+        y[:n2] = np.random.normal(0, 4.0, (n2, 1))
+        y[n2:] = np.random.normal(a, 1.0, (n2, 1))
+        c = np.zeros_like(x)
+        c[:n2] = 1
+        ybnd = xbnd * xbnd
+    elif model == 'reringnorm':
+        a = 2 / np.sqrt(2.0)
+        n2 = int(n / 2)
+        n4 = int(n / 4)
+        x[:n4] = np.random.normal(0, 4.0, (n4, 1))
+        x[n4:3 * n4] = np.random.normal(a, 1.0, (n2, 1))
+        x[3 * n4:] = np.random.normal(a, 0.25, (n4, 1))
+        y = np.array(x)
+        y[:n4] = np.random.normal(0, 4.0, (n4, 1))
+        y[n4:3 * n4] = np.random.normal(a, 1.0, (n2, 1))
+        y[n2 + n4:] = np.random.normal(a, 0.25, (n4, 1))
+        c = np.zeros_like(x)
+        c[n4:3 * n4] = 1
+        ybnd = xbnd * xbnd
+    elif model == 'xnorm':
+        a = 2 / np.sqrt(2.0)
+        n2 = int(n / 2)
+        n4 = int(n / 4)
+        x[:n2] = np.random.normal(a, 1.0, (n2, 1))
+        x[n2:] = np.random.normal(-a, 1.0, (n2, 1))
+        y = np.array(x)
+        y[:n4] = np.random.normal(a, 1.0, (n4, 1))
+        y[n4:3 * n4] = np.random.normal(-a, 1.0, (n2, 1))
+        y[3 * n4:] = np.random.normal(a, 1.0, (n4, 1))
+        c = np.ones_like(x)
+        c[:n4] = 0
+        c[n2:3 * n4] = 0
+        ybnd = xbnd * xbnd
+    elif model == 'threenorm':
+        a = 2 / np.sqrt(2.0)
+        n2 = int(n / 2)
+        n4 = int(n / 4)
+        x[:n4] = np.random.normal(a, 1.0, (n4, 1))
+        x[n4:n2] = np.random.normal(-a, 1.0, (n4, 1))
+        x[n2:] = np.random.normal(a, 1.0, (n2, 1))
+        y = np.array(x)
+        y[:n4] = np.random.normal(a, 1.0, (n4, 1))
+        y[n4:n2] = np.random.normal(-a, 1.0, (n4, 1))
+        y[n2:] = np.random.normal(-a, 1.0, (n2, 1))
+        c = np.ones_like(x)
+        c[:n2] = 0
+        ybnd = xbnd * xbnd
+    elif model == 'twonorm':
+        a = 2 / np.sqrt(2.0)
+        n2 = int(n / 2)
+        x[:n2] = np.random.normal(a, 1.0, (n2, 1))
+        x[n2:] = np.random.normal(-a, 1.0, (n2, 1))
+        y = np.array(x)
+        y[:n2] = np.random.normal(a, 1.0, (n2, 1))
+        y[n2:] = np.random.normal(-a, 1.0, (n2, 1))
+        c = np.ones_like(x)
+        c[:n2] = 0
+        ybnd = xbnd * xbnd
+    else:
+        y = np.random.rand(n, 1) * 2.0 * np.pi
+        c = y > x
+        ybnd = xbnd
+
+    y[c == True] = y[c == True] + ymargin
+    y[c == False] = y[c == False] - ymargin
+
+    if noise is not None:
+        y = y + noise * np.random.randn(n, 1)
+        x = x + noise * np.random.randn(n, 1)
+
+    if output_boundary == True:
+        return np.matlib.matrix(x), np.matlib.matrix(y), np.matlib.matrix(c * 1), xbnd, ybnd
+    else:
+        return np.matlib.matrix(x), np.matlib.matrix(y), np.matlib.matrix(c * 1)
--- main/pruebas2.py
+++ main/pruebas2.py
@@ -3,6 +3,7 @@ import sys
 # sys.path.append('/home/cromero/noise-based-classifier/')
 
 import resources.properties as properties
+import datasets.DatasetGenerator as data
 from resources.PlotModel import *
 
 from sklearn.model_selection import train_test_split
@@ -18,43 +19,13 @@ from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
 
 
-def get_data():
-    if len(sys.argv) > 1:
-        try:
-            dataset = pd.read_csv(properties.DATASET_DIRECTORY + sys.argv[1])
-            cat_columns = dataset.select_dtypes(['object']).columns
-            dataset[cat_columns] = dataset[cat_columns].astype('category')
-            dataset[cat_columns] = dataset[cat_columns].apply(lambda x: x.cat.codes)
-            dataset = dataset.values
-            X, y = dataset[:,:-1], dataset[:,-1]
-        except IOError:
-            print("File \"{}\" does not exist.".format(sys.argv[1]))
-            return
-    else:
-        X, y = make_moons(n_samples=1000, shuffle=True, noise=0.5, random_state=None)
-
-    return X, y
-
-
 def main():
-    X, y = get_data()
 
-    X_tr, X_te, y_train, y_test = train_test_split(X, y, test_size=0.33)
+    X_test, y_test = data.create_dataset(5000, "twonorm")
 
-    ###################################
-    #####      NORMALIZATION      #####
-    ###################################
-    sc = StandardScaler()
-    X_tr_std = sc.fit_transform(X_tr)
-    X_te_std = sc.transform(X_te)
+    print(X_test)
 
-    #########################################
-    #####      DATA TRANSFORMATION      #####
-    #########################################
-    pca = PCA(n_components=2)
-
-    X_train = pca.fit_transform(X_tr_std)
-    X_test = pca.transform(X_te_std)
+    return
 
     #########################################
     #####      DATA CLASSIFICATION      #####
