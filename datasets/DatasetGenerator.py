0675e21bd5bde51ea5d678e2d9fa60e2900d0cd7
Mario <mar294cr@gmail.com>
2019-11-21 17:13:13 +0100

    dataset_generator

--- datasets/DatasetGenerator.py
+++ datasets/DatasetGenerator.py
@@ -97,3 +97,47 @@ def create_dataset(n, model, ymargin=0.0, noise=None, output_boundary=False):
         return np.matlib.matrix(x), np.matlib.matrix(y), np.matlib.matrix(c * 1), xbnd, ybnd
     else:
         return np.matlib.matrix(x), np.matlib.matrix(y), np.matlib.matrix(c * 1)
+
+
+def create_full_dataset(n, dimm, model, noise=None):
+    x = np.random.rand(n, dimm) * 2.0 * np.pi
+
+    if model == 'threenorm':
+        a = 2 / np.sqrt(2.0)
+
+        n2 = int(n / 2)
+        n4 = int(n / 4)
+
+        for i in range(dimm):
+            x[:n4, i] = np.random.normal(a, 1.0, (n4, 1)).transpose()
+            x[n4:n2, i] = np.random.normal(-a, 1.0, (n4, 1)).transpose()
+
+            if i % 2 == 0:
+                x[n2:, i] = np.random.normal(a, 1.0, (n2, 1)).transpose()
+            else:
+                x[n2:, i] = np.random.normal(-a, 1.0, (n2, 1)).transpose()
+
+        # x[:n4, :] = np.random.normal(a, 1.0, (n4, 1))
+        # x[n4:n2, :] = np.random.normal(-a, 1.0, (n4, 1))
+        # x[n2:, ::2] = np.random.normal(a, 1.0, (n2, 1))
+        # x[n2:, 1::2] = np.random.normal(-a, 1.0, (n2, 1))
+
+        c = np.ones((1, n))
+        c[:, :n2] = 0
+
+    elif model == "ringnorm":
+        a = 2 / np.sqrt(2.0)
+
+        n2 = int(n / 2)
+
+        for i in range(dimm):
+            x[:n2, i] = np.random.normal(0, 4.0, (n2, 1)).transpose()
+            x[n2:, i] = np.random.normal(a, 1.0, (n2, 1)).transpose()
+
+        c = np.ones((1, n))
+        c[:, :n2] = 0
+
+    if noise is not None:
+        x = x + noise * np.random.randn(n, 1)
+
+    return np.matlib.matrix(x), np.matlib.matrix(c * 1)
